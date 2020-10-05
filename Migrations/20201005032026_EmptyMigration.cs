8c553c7d5693fb09ee405eb46fe111f5165f5d46
Kyle Adams <kadams54@users.noreply.github.com>
2020-10-04 23:24:27 -0400

    Add what ought to be an empty migration
    No changes were made, so the added migration should be empty. That said,
    as can be seen in the migration, EF Core 3.1 generates an `UpdateData`
    call for the boolean field. That `UpdateData` doesn't actually change
    the field and isn't necessary.

---
+++ Migrations/20201005032026_EmptyMigration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoApi.Migrations
+{
+    public partial class EmptyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TodoItems",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "IsComplete",
+                value: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TodoItems",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "IsComplete",
+                value: true);
+        }
+    }
+}
